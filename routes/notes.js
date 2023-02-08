74068a2ca02ced018ea314ff396ae989d685f615
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2023-02-08 00:37:37 -0500

    fixed bugs and added delete/save code in notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,33 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
-
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
 notes.get('/', (req, res) => {
   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
+notes.get('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.id !== noteId);
+      writeToFile('./db/notes.json', result);
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
 
 notes.post('/', (req, res) => {
   console.log(req.body);
@@ -18,7 +39,7 @@ notes.post('/', (req, res) => {
         const newNote = {
             title,
             text,
-            id: uuid(),
+            id: uuidv4(),
             
         };
 
